ecc33467ae3db4e61ae3ed420d98ceb9f2cb9dd7
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-26 15:17:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,13 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
   end
   
 end
