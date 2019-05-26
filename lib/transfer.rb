5756497cf5d28c6c0524f2ffabb92a31c38ed730
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-26 15:13:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   def execute_transaction
      if valid? && sender.balance > amount
-       sender.balance - amount
+       sender.balance -= amount
      receiver.balance + @amount
   end
   
